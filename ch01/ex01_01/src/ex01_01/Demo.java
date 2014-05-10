d17778ec1b656bf4827b0b9507d93a3e16cdcf83
zDpxq6 <tsuguka@gmail.com>
2014-05-10 21:01:33 +0900

    更新

---
+++ ch01/ex01_01/src/ex01_01/Demo.java
@@ -0,0 +1,8 @@
+package ex01_01;
+
+public class Demo {
+	//練習1.1 あなたが使用しているシステムでHelloWorldを入力、コンパイル、そして実行して下さい。
+	public static void main(String[] args){
+		System.out.println("Hello, world");
+	}
+}
