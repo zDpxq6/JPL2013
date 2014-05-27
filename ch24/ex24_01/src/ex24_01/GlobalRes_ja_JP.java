4d0f2a566ea2956fdd1bead93c925ed39e1a0c30
zDpxq6 <tsuguka@gmail.com>
2014-05-27 22:42:16 +0900

    更新

---
+++ ch24/ex24_01/src/ex24_01/GlobalRes_ja_JP.java
@@ -0,0 +1,14 @@
+package ex24_01;
+
+import java.util.ListResourceBundle;
+
+public class GlobalRes_ja_JP extends ListResourceBundle {
+
+	@Override
+	protected Object[][] getContents() {
+		return contents;
+	}
+
+	private static final Object[][] contents = { { GlobalRes.HELLO, "こんにちは" }, { GlobalRes.GOODBYE, "さようなら" }, };
+
+}
