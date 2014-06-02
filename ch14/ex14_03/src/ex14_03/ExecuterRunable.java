6fbc6f0f3dfddb96e463eca57b8a2ace2e433dce
zDpxq6 <tsuguka@gmail.com>
2014-06-02 23:09:43 +0900

    更新

--- ch14/ex14_03/src/ex14_03/ExecuterRunable.java
+++ ch14/ex14_03/src/ex14_03/ExecuterRunable.java
@@ -1,5 +1,8 @@
 //(C) 2014 zDpxq6
 package ex14_03;
+//現在の値を保持し、その値に加算して新たな値を表示するメソッドを持つオブジェクトのクラスを作成しなさい。
+//そのオブジェクトを作成し、複数のスレッドを生成して、各スレッドからその加算メソッドを繰り返し呼び出すプログラムを作成しなさい。
+//加算の結果が失われないようにそのクラスを作成しなさい。
 
 public class ExecuterRunable implements Runnable{
 	private static final int MAX = 100;
