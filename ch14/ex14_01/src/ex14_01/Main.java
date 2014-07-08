48561c9d97e35edff5df146fd0d64a7a62fb81ad
tsuguka <tsuguka.hatanaka@nst.ricoh.co.jp>
2014-07-08 11:56:43 +0900

    git commit test

--- ch14/ex14_01/src/ex14_01/Main.java
+++ ch14/ex14_01/src/ex14_01/Main.java
@@ -4,6 +4,7 @@ package ex14_01;
 public class Main {
 	//練習問題14.1 mainを実行しているスレッドの名前を表示するプログラムを作成しなさい。
 	public static void main(String[] args){
+		//test
 		System.out.println(Thread.currentThread().getName());
 	}
 }
